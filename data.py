e34b3b918741a47f4c48f410a929d4d5f813ae63
Asliddin <asliddinchoriyev0828@gmail.com>
2024-10-26 23:08:50 +0500

    first commit

---
+++ data.py
@@ -0,0 +1,67 @@
+data=[ 
+    {
+      "title": "ZIRO",
+      "link": "https://freelancer-uz.vercel.app"
+    },
+    {
+      "title": "Payments app",
+      "link": "https://payments-uz.vercel.app"
+    },
+    {
+      "title": "MyNotes",
+      "link": "https://my-notes-uz.vercel.app"
+    },
+    {
+      "title": "Calculator",
+      "link": "https://calculatoruz.vercel.app"
+    },
+    {
+      "title": "Dark Lamps",
+      "link": "https://dark-lamps.vercel.app"
+    },
+    {
+      "title": "Furniture",
+      "link": "https://furniture-uz.vercel.app"
+    },
+    {
+      "title": "GoTriep",
+      "link": "https://gotrip-uz.vercel.app"
+    },
+    {
+      "title": "Dashboard",
+      "link": "https://dashboard-uz.vercel.app"
+    },
+    {
+      "title": "Interior Market",
+      "link": "https://interior-market-uz.vercel.app"
+    },
+    {
+      "title": "InteriorQU",
+      "link": "https://interior-uz.vercel.app"
+    },
+    {
+      "title": "Taylor's Portfolio",
+      "link": "https://taylors-portfolio-from-asliddin.vercel.app"
+    },
+    {
+      "title": "Scandleted",
+      "link": "https://scandleted.vercel.app"
+    },
+    {
+      "title": "Qr Code Generator",
+      "link": "https://qrcode-uz.vercel.app"
+    },
+    {
+      "title": "Universal Randomer",
+      "link": "https://random-uz.vercel.app"
+    },
+    {
+      "title": "Quiz App (Demo)",
+      "link": "https://quizapp-uz.vercel.app"
+    },
+    {
+      "title": "BookShelf",
+      "link": "https://bookshelf-uz.vercel.app"
+    }
+  ]
+  
---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request
+from data import data
+
+app = Flask(__name__)
+
+# Ma'lumotlar bazasi sifatida oddiy ro'yxat
+
+# GET so'rovi
+@app.route('/items', methods=['GET'])
+def get_items():
+    return jsonify(data)
+
+# POST so'rovi
+@app.route('/items', methods=['POST'])
+def add_item():
+    item = request.json
+    data.append(item)
+    return jsonify(item), 201
+
+# API serverini ishga tushirish
+if __name__ == '__main__':
+    app.run(debug=True)
+
