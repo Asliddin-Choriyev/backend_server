3868134f14b0839cd99c53ebc2d92ae86f869366
Asliddin <asliddinchoriyev0828@gmail.com>
2024-10-27 10:43:25 +0500

    c

--- data.py
+++ data.py
@@ -1,67 +1,18 @@
-data=[ 
-    {
-      "title": "ZIRO",
-      "link": "https://freelancer-uz.vercel.app"
-    },
-    {
-      "title": "Payments app",
-      "link": "https://payments-uz.vercel.app"
-    },
-    {
-      "title": "MyNotes",
-      "link": "https://my-notes-uz.vercel.app"
-    },
-    {
-      "title": "Calculator",
-      "link": "https://calculatoruz.vercel.app"
-    },
-    {
-      "title": "Dark Lamps",
-      "link": "https://dark-lamps.vercel.app"
-    },
-    {
-      "title": "Furniture",
-      "link": "https://furniture-uz.vercel.app"
-    },
-    {
-      "title": "GoTriep",
-      "link": "https://gotrip-uz.vercel.app"
-    },
-    {
-      "title": "Dashboard",
-      "link": "https://dashboard-uz.vercel.app"
-    },
-    {
-      "title": "Interior Market",
-      "link": "https://interior-market-uz.vercel.app"
-    },
-    {
-      "title": "InteriorQU",
-      "link": "https://interior-uz.vercel.app"
-    },
-    {
-      "title": "Taylor's Portfolio",
-      "link": "https://taylors-portfolio-from-asliddin.vercel.app"
-    },
-    {
-      "title": "Scandleted",
-      "link": "https://scandleted.vercel.app"
-    },
-    {
-      "title": "Qr Code Generator",
-      "link": "https://qrcode-uz.vercel.app"
-    },
-    {
-      "title": "Universal Randomer",
-      "link": "https://random-uz.vercel.app"
-    },
-    {
-      "title": "Quiz App (Demo)",
-      "link": "https://quizapp-uz.vercel.app"
-    },
-    {
-      "title": "BookShelf",
-      "link": "https://bookshelf-uz.vercel.app"
-    }
-  ]
-  
+data = [
+    {"link": "https://freelancer-uz.vercel.app", "title": "ZIRO"},
+    {"link": "https://payments-uz.vercel.app", "title": "Payments app"},
+    {"link": "https://my-notes-uz.vercel.app", "title": "MyNotes"},
+    {"link": "https://calculatoruz.vercel.app", "title": "Calculator"},
+    {"link": "https://dark-lamps.vercel.app", "title": "Dark Lamps"},
+    {"link": "https://furniture-uz.vercel.app", "title": "Furniture"},
+    {"link": "https://gotrip-uz.vercel.app", "title": "GoTriep"},
+    {"link": "https://dashboard-uz.vercel.app", "title": "Dashboard"},
+    {"link": "https://interior-market-uz.vercel.app", "title": "Interior Market"},
+    {"link": "https://interior-uz.vercel.app", "title": "InteriorQU"},
+    {"link": "https://taylors-portfolio-from-asliddin.vercel.app", "title": "Taylor's Portfolio"},
+    {"link": "https://scandleted.vercel.app", "title": "Scandleted"},
+    {"link": "https://qrcode-uz.vercel.app", "title": "Qr Code Generator"},
+    {"link": "https://random-uz.vercel.app", "title": "Universal Randomer"},
+    {"link": "https://quizapp-uz.vercel.app", "title": "Quiz App (Demo)"},
+    {"link": "https://bookshelf-uz.vercel.app", "title": "BookShelf"},
+]
